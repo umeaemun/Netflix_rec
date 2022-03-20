3fed6e872ab5daf70f206badc4e19162c2f20d5e
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-03-20 17:33:25 +0500

    done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import './App.css';
 import Card from './Card.js';
 import Sdata from './Sdata';
 
-let ncard=(val)=>{
-  return(
-    <Card 
-    imgsrc={val.imgsrc}
-    category={val.category} 
-    title={val.title}
-    link={val.link}
-    />
-  );
-}
 
 function App() {
   return (
     <> 
     <h1 className='heading_style'> List of Top 3 Netflix 2022 </h1>
-    {Sdata.map(ncard)}    
+
+    {Sdata.map((val) => {
+      return(
+         <Card 
+         imgsrc={val.imgsrc}
+         category={val.category} 
+         title={val.title}
+         link={val.link}
+         />
+        );                               
+      })}  
+
     </>
       );
 }
