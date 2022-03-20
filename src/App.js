bc88166c18844cc904dbaa6fcfc28975f73a8c36
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-03-20 17:25:35 +0500

    done

--- src/App.js
+++ src/App.js
@@ -2,35 +2,25 @@ import './App.css';
 import Card from './Card.js';
 import Sdata from './Sdata';
 
+let ncard=(val)=>{
+  return(
+    <Card 
+    imgsrc={val.imgsrc}
+    category={val.category} 
+    title={val.title}
+    link={val.link}
+    />
+  );
+}
+
 function App() {
   return (
     <> 
-
     <h1 className='heading_style'> List of Top 3 Netflix 2022 </h1>
-
-    <Card 
-    imgsrc={Sdata[0].imgsrc}
-    category={Sdata[0].category} 
-    title={Sdata[0].title}
-    link={Sdata[0].link}
-    />
-
-    <Card 
-        imgsrc={Sdata[1].imgsrc}
-        category={Sdata[1].category} 
-        title={Sdata[1].title}
-        link={Sdata[1].link}
-        />
-
-    <Card 
-        imgsrc={Sdata[2].imgsrc}
-        category={Sdata[2].category} 
-        title={Sdata[2].title}
-        link={Sdata[2].link}
-        />
-        
+    {Sdata.map(ncard)}    
     </>
       );
 }
 
 export default App;
+
